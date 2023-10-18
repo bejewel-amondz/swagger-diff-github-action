38de7ed53b960e499a11ca598e9d3588b581817a
st <st@bejewel.kr>
2023-10-18 23:13:30 +0900

    init commit

---
+++ src/functions.js
@@ -0,0 +1,46 @@
+import { structuredPatch } from "diff";
+
+/**
+ * Get pull requests from multiple github repositories
+ * @param {*} source
+ * @param {*} target
+ * @param {*} repos
+ * @returns
+ */
+export function compareJsonsWithStructured(source, target) {
+    return structuredPatch('file1.json','file2.json', JSON.stringify(source.json, null, 2), JSON.stringify(target.json, null, 2));
+}
+
+export function diffToHtml(diff) {
+    let html = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>JSON Diff</title>
+    </head>
+    <body>
+    <div style="font-family: monospace;">
+    `;
+
+    diff.hunks.forEach(hunk => {
+        html += `<div><strong>file1.json 의 Lines ${hunk.oldStart}-${hunk.oldStart + hunk.oldLines - 1}:</strong></div>`;
+        html += '<div style="background-color: #f6f8fa; padding: 10px; border: 1px solid #ddd; margin-bottom: 10px;">';
+        hunk.lines.forEach(line => {
+            if (line.startsWith('-')) {
+                html += `<div style="color: red;">${line}</div>`;
+            } else if (line.startsWith('+')) {
+                html += `<div style="color: green;">${line}</div>`;
+            } else {
+                html += `<div style="color: grey;">${line}</div>`;
+            }
+        });
+        html += '</div>';
+    });
+
+    html += '</div></body></html>';
+
+    return html;
+}
---
+++ src/index.js
@@ -0,0 +1,54 @@
+import core from '@actions/core';
+import artifact from '@actions/artifact';
+import { compareJsonsWithStructured, diffToHtml } from "./functions.js";
+// import * as fs from "fs";
+
+async function main() {
+    try {
+        const sourceSwaggerDocsJson = core.getInput('source-swagger-docs-json', {
+            required: true,
+        });
+        // const sourceSwaggerDocsJson = JSON.parse(fs.readFileSync('./file1.json', 'utf8'));
+        const targetSwaggerDocsJson = core.getInput('target-swagger-docs-json', {
+            required: true,
+        });
+        // const targetSwaggerDocsJson = JSON.parse(fs.readFileSync('./file2.json', 'utf8'));
+
+        // json 비교
+        const diff = compareJsonsWithStructured({
+            json: sourceSwaggerDocsJson
+        }, {
+                json: targetSwaggerDocsJson
+            }
+            );
+
+        // json 의 변경된 사항을 HTML 형식으로 변환하기
+        const htmlContent = diffToHtml(diff);
+        const contentBuffer = Buffer.from(htmlContent, "utf-8");
+
+        const artifactName = "my-artifact";
+        const fileName = "buffer-content.html";
+
+        const file = {
+            path: fileName,
+            size: contentBuffer.length,
+            file: contentBuffer
+        };
+
+        const artifactClient = artifact.create();
+        const uploadResponse = await artifactClient.uploadArtifact(artifactName, [file], __dirname, {
+            continueOnError: false
+        });
+        if (uploadResponse.failedItems.length > 0) {
+            core.setFailed('Some items failed to upload');
+        } else {
+            core.info('Artifact uploaded successfully');
+        }
+
+    } catch (error) {
+        console.error(error);
+        core.setFailed(error.message);
+    }
+}
+
+main();
